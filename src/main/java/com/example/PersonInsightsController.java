6eb13c065801ae42a65c73a46b0df80fe30bef67
Karthikeyan Balasubramaniam <nklkarthi@gmail.com>
2016-06-06 20:50:00 +0800

    Working with Spring Boot and Rest

---
+++ src/main/java/com/example/PersonInsightsController.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.watson.developer_cloud.personality_insights.v2.PersonalityInsights;
+import com.ibm.watson.developer_cloud.personality_insights.v2.model.Profile;
+
+@RestController
+public class PersonInsightsController {
+
+	@RequestMapping("/getPersonInsights")
+	public Profile getPersonalityInsights() {
+		PersonalityInsights service = new PersonalityInsights();
+		service.setUsernameAndPassword("50483c2d-c2ff-4116-9389-0df194d8eca9", "KQjVEgpCDZ54");
+
+		String text = "Call me Ishmael. Some years ago-never mind how long "
+				+ "precisely-having little or no money in my purse, and nothing "
+				+ "particular to interest me on shore, I thought I would sail about "
+				+ "a little and see the watery part of the world. It is a way "
+				+ "I have of driving off the spleen and regulating the circulation. "
+				+ "Whenever I find myself growing grim about the mouth; whenever it "
+				+ "is a damp, drizzly November in my soul; whenever I find myself "
+				+ "involuntarily pausing before coffin warehouses, and bringing up "
+				+ "the rear of every funeral I meet; and especially whenever my "
+				+ "hypos get such an upper hand of me, that it requires a strong "
+				+ "moral principle to prevent me from deliberately stepping into "
+				+ "the street, and methodically knocking people's hats off-then, "
+				+ "I account it high time to get to sea as soon as I can.";
+
+		Profile profile = service.getProfile(text).execute();
+		return profile;
+	}
+}
---
+++ src/main/java/com/example/ToneAnalyzerController.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.watson.developer_cloud.tone_analyzer.v3_beta.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3_beta.model.ToneAnalysis;
+
+@RestController
+public class ToneAnalyzerController {
+
+	@RequestMapping("/getToneAnalysis")
+	public ToneAnalysis getToneAnalyzer() {
+
+		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_02_11);
+		service.setUsernameAndPassword("5c592faa-fca8-4dd7-805d-0393f83150ca", "oMq0MFQ0cmcY");
+
+		String text = "I know the times are difficult! Our sales have been "
+				+ "disappointing for the past three quarters for our data analytics "
+				+ "product suite. We have a competitive data analytics product "
+				+ "suite in the industry. But we need to do our job selling it! "
+				+ "We need to acknowledge and fix our sales challenges. "
+				+ "We can’t blame the economy for our lack of execution! "
+				+ "We are missing critical sales opportunities. "
+				+ "Our product is in no way inferior to the competitor products. "
+				+ "Our clients are hungry for analytical tools to improve their "
+				+ "business outcomes. Economy has nothing to do with it.";
+
+		// Call the service and get the tone
+		ToneAnalysis tone = service.getTone(text).execute();
+		return tone;
+
+	}
+}
