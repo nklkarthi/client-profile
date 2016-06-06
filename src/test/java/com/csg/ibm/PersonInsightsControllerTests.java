b687107468a7519a71e54f2b88fe9534c72b27cb
Karthikeyan Balasubramaniam <nklkarthi@gmail.com>
2016-06-06 23:13:13 +0800

    Tests file updated

--- src/test/java/com/csg/ibm/PersonInsightsControllerTests.java
+++ src/test/java/com/csg/ibm/PersonInsightsControllerTests.java
@@ -1,9 +1,11 @@
-package com.example;
+package com.csg.ibm;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.csg.ibm.PersonInsightsController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PersonInsightsController.class)
